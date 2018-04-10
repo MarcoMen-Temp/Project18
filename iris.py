cd85a17506fe4e380b00c44ac28a2ebca42e9e58
Marco Men <mrc_men@yahoo.ie>
2018-04-11 00:19:36 +0100

    Added code

--- iris.py
+++ iris.py
@@ -2,12 +2,24 @@
 # This file will read,retrieve and manipulate data from 'iris.csv'
 
 
-import pandas as pd
 
-df = pd.read_csv('iris.csv', sep= ',' , names = ['SepalWidth(cm)', 'SepalLenght(cm)', 'PetalWidth(cm)', 'PetalLenght(cm)', 'Species']) #Stack OverFlow:'How to add header row to pandas DataFrame
+
+import pandas as pd
+import numpy as np
+
+
+df = pd.read_csv('iris.csv', header=None) # StackOverflow: 'How to add header row to pandas DataFrame and PythonHow.com-'Loading CSV data in Python with pandas'
+df.columns = ['Sep_L', 'Sep_W', 'Pet_L', 'Pet_W', 'Species']
 
 print(df.head())
 
-print (df.shape)
 
 print (df.columns)
+#Assigning variables to header row(Index),to facilitate statistical calculations of the 4 numerical rows
+SepLength = df ['Sep_L' ]
+SepWidth = df ['Sep_W']
+
+
+print(SepLength.describe()) # Statistical calculations for 1st column -Pg.73
+print(SepWidth.describe())
+
