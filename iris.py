3c8fef097c6314e6611dbe6af9ce5c50cda3c985
Marco Men <mrc_men@yahoo.ie>
2018-04-10 16:29:55 +0100

    added code

--- iris.py
+++ iris.py
@@ -1,2 +1,13 @@
 # Marco Men - Project2018
 # This file will read,retrieve and manipulate data from 'iris.csv'
+
+
+import pandas as pd
+
+df = pd.read_csv('iris.csv', sep= ',')
+
+print(df.head())
+
+print (df.shape)
+
+print (df.columns)
