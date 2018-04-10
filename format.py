13ba190d1a855f88dfeda9cf0328716896fe6ad4
Marco Men <mrc_men@yahoo.ie>
2018-04-11 00:20:46 +0100

    Transfered from a different folder

---
+++ format.py
@@ -0,0 +1,14 @@
+"""Marco Men - Exercise 5 (Format)
+Adapted from StackOverFlow: 'Formatting Output of CSV file in Python' """ 
+import csv
+
+def concat(*args, sep= "|"):
+      return sep.join(args)
+
+
+print(concat("SepalLength","SepalWidth","PetalLength","PetalWidth","Species"))
+
+with open("iris.csv") as f:
+      reader = csv.reader(f)
+      for row in reader:
+            print('{:>10} | {:>8} | {:>9} | {:>8} | {:>9} ' .format(*row))
