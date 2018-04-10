a88dbb5ddb3858cdbe1c444c7c7b3f1d4e768cbb
Marco Men <mrc_men@yahoo.ie>
2018-04-10 16:12:57 +0100

    Added comments

---
+++ iris.py
@@ -0,0 +1,2 @@
+# Marco Men - Project2018
+# This file will read,retrieve and manipulate data from 'iris.csv'
---
+++ plot.py
@@ -0,0 +1,2 @@
+# Marco Men - Project2018
+# With this file, plots will be crated using data
