c0fea4867be9750c46f11c2d2cec5b0e7bc6f1a9
Marco Men <mrc_men@yahoo.ie>
2018-04-11 00:30:29 +0100

    Added more code

--- iris.py
+++ iris.py
@@ -11,15 +11,22 @@ import numpy as np
 df = pd.read_csv('iris.csv', header=None) # StackOverflow: 'How to add header row to pandas DataFrame and PythonHow.com-'Loading CSV data in Python with pandas'
 df.columns = ['Sep_L', 'Sep_W', 'Pet_L', 'Pet_W', 'Species']
 
-print(df.head())
+print(df.head()) # Prints the first 5 rows
 
 
-print (df.columns)
+print (df.columns) #Get column names on an Index Line(in Terminal)
 #Assigning variables to header row(Index),to facilitate statistical calculations of the 4 numerical rows
 SepLength = df ['Sep_L' ]
 SepWidth = df ['Sep_W']
+PetLength = df ['Pet_L']
+PetWidth = df ['Pet_W']
 
 
-print(SepLength.describe()) # Statistical calculations for 1st column -Pg.73
+print(SepLength.describe()) # Statistical calculations(Summary of Statistics) for  columns using  'describe'method() -Pg.73
+
 print(SepWidth.describe())
+
+print(PetLength.describe())
+
+print(PetWidth.describe())
 
