efbe9d8cc5f7fc67a26dfeb6dcc8e658b1cf8ed1
Marco Men <mrc_men@yahoo.ie>
2018-04-12 13:33:19 +0100

    Added code

--- iris.py
+++ iris.py
@@ -6,7 +6,8 @@
 
 import pandas as pd
 import numpy as np
-
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 df = pd.read_csv('iris.csv', header=None) # StackOverflow: 'How to add header row to pandas DataFrame and PythonHow.com-'Loading CSV data in Python with pandas'
 df.columns = ['Sep_L', 'Sep_W', 'Pet_L', 'Pet_W', 'Species']
@@ -21,12 +22,52 @@ SepWidth = df ['Sep_W']
 PetLength = df ['Pet_L']
 PetWidth = df ['Pet_W']
 
-
+# Histograms are the most common means of looking at a single variable(univariate)
 print(SepLength.describe()) # Statistical calculations(Summary of Statistics) for  columns using  'describe'method() -Pg.73
+
+fig = plt.figure()
+axes1 = fig.add_subplopt(1,1,1)# 2 graph rows(1- Histogram;2- Boxplot)
+axes1.hist(SepLength[df ['Sep_L'], bins= 10)
+axes1.set_title('Histogram of Sepal Lenght(cm)')
+axes1.set_xlabel('Frequency')
+axes1.set_ylabel('Bin')
+fig.show ( )
+
+#Boxplot next
+boxplot = plt.figure( )
+axes1 = boxplot.add_subplot(1,1,1)
+axes1.boxplot([df['Species'] == 'Iris Setosa'] ['Lenght'], 
+df['Species'] == 'Iris Versicolor'] ['Lenght'], 
+df['Species'] == 'Iris Virginica'] ['Lenght'])
+
+Iris Setosa,Iris Versicolor,Iris Virginica
 
 print(SepWidth.describe())
 
+fig = plt.figure()
+axes1 = fig.add_subplopt(1,1,1)# 2 graph rows(1- Histogram;2- Boxplot)
+axes1.hist(SepLength[df ['Sep_W'], bins= 10)
+axes1.set_title('Histogram of Sepal Width(cm)')
+axes1.set_xlabel('Frequency')
+axes1.set_ylabel('Bin')
+fig.show ( )
+
 print(PetLength.describe())
+
+fig = plt.figure()
+axes1 = fig.add_subplopt(1,1,1)# 2 graph rows(1- Histogram;2- Boxplot)
+axes1.hist(SepLength[df ['Pet_L'], bins= 10)
+axes1.set_title('Histogram of Petal Lenght(cm)')
+axes1.set_xlabel('Frequency')
+axes1.set_ylabel('Bin')
+fig.show ( )
 
 print(PetWidth.describe())
 
+fig = plt.figure()
+axes1 = fig.add_subplopt(1,1,1)# 2 graph rows(1- Histogram;2- Boxplot)
+axes1.hist(SepLength[df ['Pet_W'], bins= 10)
+axes1.set_title('Histogram of Petal Width(cm)')
+axes1.set_xlabel('Frequency')
+axes1.set_ylabel('Bin')
+fig.show ( )
