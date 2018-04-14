f32b4f390937b6cc6a9c2612a942a1fc987f0187
Marco Men <mrc_men@yahoo.ie>
2018-04-14 23:32:23 +0100

    Deleted code

--- format.py
+++ format.py
@@ -12,3 +12,8 @@ with open("iris.csv") as f:
       reader = csv.reader(f)
       for row in reader:
             print('{:>10} | {:>8} | {:>9} | {:>8} | {:>9} ' .format(*row))
+
+df.to_csv('iris_df.csv' ,sep='|')
+
+
+df1 = pd.read_csv('iris_df.csv')
