224af4e5d63c6e001c6cb192e6b82e3634fbcd56
Marco Men <mrc_men@yahoo.ie>
2018-04-18 21:27:11 +0100

    Finished plots

--- boxplot.py
+++ boxplot.py
@@ -5,25 +5,9 @@ import matplotlib.pyplot as plt
 
 import pandas as pd
 
+iris = sns.load_dataset('iris')
 
-df = pd.read_csv('iris.csv', header = None)
+print(iris)
 
-df1 = df.iloc[0:50:1]
-df2 = df.iloc[50:100:1]
-df3 = df.iloc[100:150:1]
-
-print(df1)
-print(df1.describe())
-
-print(df2)
-print(df2.describe())
-
-print(df3)
-print(df3.describe())
-
-box, ax = plt.subplots(1,1,1 )
-ax = sns.boxplot(x= 'Species', y= 'Data', data= df)
-ax.set_title ('Boxplot')
-ax.set_xlabel('Species')
-ax.set_ylabel('Data')
+ax = sns.boxplot(data = iris, hue= 'species', orient= 'v', palette='Set2')  #https://seaborn.pydata.org- 'Seaborn.boxplot
 plt.show()
