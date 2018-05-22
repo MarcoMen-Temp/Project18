764c9a63696c8a8c4610c93de2e5b29e33e8e391
Marco Men <mrc_men@yahoo.ie>
2018-05-22 10:52:43 +0100

    changed length word

--- LinReg.py
+++ LinReg.py
@@ -3,14 +3,14 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('iris.csv', header = None) # StackOverflow: 'How to add header row to pandas DataFrame and PythonHow.com-'Loading CSV data in Python with pandas
-df.columns = ['Sepal Lenght','Sepal Width','Petal Lenght','Petal Width','Species']
+df.columns = ['Sepal Length','Sepal Width','Petal Length','Petal Width','Species']
 
 
 
-fig = sns.lmplot (x= 'Sepal Lenght', y= 'Sepal Width', data= df, hue= 'Species')
+fig = sns.lmplot (x= 'Sepal Length', y= 'Sepal Width', data= df, hue= 'Species')
 plt.show()
 
 
-fig = sns.lmplot (x= 'Petal Lenght', y= 'Petal Width', data= df, hue= 'Species')
+fig = sns.lmplot (x= 'Petal Length', y= 'Petal Width', data= df, hue= 'Species')
 plt.show()
 
